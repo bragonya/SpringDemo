b2dd9ad66cc50a5c634e57a66a197f9a1c25b218
fernando <rluis4490@gmail.com>
2019-06-20 23:21:10 -0600

    find car by id, and get all cars complete

--- carservice/src/main/java/com/brayan/carservice/CarServiceApplication.java
+++ carservice/src/main/java/com/brayan/carservice/CarServiceApplication.java
@@ -1,7 +1,12 @@
 package com.brayan.carservice;
 
+import com.brayan.carservice.models.Car;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CarServiceApplication {
@@ -10,4 +15,15 @@ public class CarServiceApplication {
 		SpringApplication.run(CarServiceApplication.class, args);
 	}
 
+
+	@Bean
+	public List<Car> getListCar()
+	{
+		return Arrays.asList(
+				new Car("Toyota", 2019, 1),
+				new Car("Honda", 2017, 2),
+				new Car("Isuzu", 2011, 3),
+				new Car("Mazda", 2010, 4)
+		);
+	}
 }
