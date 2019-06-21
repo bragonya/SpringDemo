42f82e83ffecfb47f12420d5e0e24d86ae8c6a47
bragonya <bragonya@gmail.com>
2019-06-21 00:07:30 -0600

    user service is discovered by eureka

--- serverdiscovery/src/main/java/com/brayan/serverdiscovery/ServerDiscoveryApplication.java
+++ serverdiscovery/src/main/java/com/brayan/serverdiscovery/ServerDiscoveryApplication.java
@@ -2,12 +2,15 @@ package com.brayan.serverdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class ServerdiscoveryApplication {
+@EnableEurekaServer
+public class ServerDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerdiscoveryApplication.class, args);
+
+		SpringApplication.run(ServerDiscoveryApplication.class, args);
 	}
 
 }
