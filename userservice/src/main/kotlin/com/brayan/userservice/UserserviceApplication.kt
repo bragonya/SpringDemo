403aac312b1bc91416e3976f321a8e033cc42e24
bragonya <bragonya@gmail.com>
2019-06-21 00:47:08 -0600

    orchestation car service and user service

--- userservice/src/main/kotlin/com/brayan/userservice/UserserviceApplication.kt
+++ userservice/src/main/kotlin/com/brayan/userservice/UserserviceApplication.kt
@@ -3,16 +3,26 @@ package com.brayan.userservice
 import com.brayan.userservice.models.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
+@EnableEurekaClient
 class UserserviceApplication{
 
 	@Bean
 	fun provideUsers():List<User>{
 		return listOf(User("Brayan",1,12, listOf(1,2,3,4)),
-				User("Ana",1,12, listOf(1)),
-				User("Joaquin",1,12, listOf(3,4)))
+				User("Ana",2,12, listOf(1)),
+				User("Joaquin",3,12, listOf(3,4)))
+	}
+
+	@Bean
+	@LoadBalanced
+	fun provideRestTemplate():RestTemplate{
+		return RestTemplate()
 	}
 }
 
--- userservice/src/main/kotlin/com/brayan/userservice/resources/CarsByUserResource.kt
+++ userservice/src/main/kotlin/com/brayan/userservice/resources/CarsByUserResource.kt
@@ -1,28 +1,29 @@
 package com.brayan.userservice.resources
 
+import com.brayan.userservice.models.Car
 import com.brayan.userservice.models.User
-import com.netflix.discovery.converters.Auto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
-
 @RestController
-@RequestMapping("/user")
-class UserListResource {
+@RequestMapping("/carsbyuser")
+class CarsByUserResource {
+
+    @Autowired
+    private lateinit var restTemplate: RestTemplate
 
     @Autowired
     private lateinit var users:List<User>
+    @RequestMapping("/getCarsOfUsesr/{userId}")
+    fun getCarsByUser(@PathVariable("userId") userId:Int):List<Car?>?{
 
-    @RequestMapping("/allUser")
-    fun allUsers():List<User>{
+        return users.find { it.id == userId }?.carId?.map {
+            restTemplate.getForObject("http://car-service/car/findCar/"+it,Car::class.java)
+        }
 
-        return users
     }
 
-    @RequestMapping("/findUser/{userId}")
-    fun findUser(@PathVariable("userId") id:Int):User?{
-        return users.find { it.id == id }
-    }
+
 }
